38575ec323123c2e3031d8c25f657ebd97076b75
isadrianus <adrianus.isa@modena.com>
2023-10-12 15:16:31 +0700

    add: StudentServiceController

---
+++ api/interfaces/studentController.go
@@ -0,0 +1,289 @@
+package interfaces
+
+import (
+	"alfath_lms/api/definitions"
+	"alfath_lms/api/deps/validator"
+	"alfath_lms/api/funcs"
+	"alfath_lms/api/student/domain/entity"
+	"alfath_lms/api/student/domain/service"
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"flamingo.me/flamingo/v3/framework/web"
+)
+
+type (
+	StudentController struct {
+		responder       *web.Responder
+		customValidator *validator.CustomValidator
+		studentService  service.StudentServiceInterface
+	}
+
+	GetStudentResponse struct {
+		Status int
+		Data   entity.Student
+	}
+)
+
+func (studentController *StudentController) Inject(
+	responder *web.Responder,
+	customValidator *validator.CustomValidator,
+	studentService service.StudentServiceInterface,
+) {
+	studentController.responder = responder
+	studentController.customValidator = customValidator
+	studentController.studentService = studentService
+}
+
+func (studentController *StudentController) Create(ctx context.Context, req *web.Request) web.Result {
+	formError := req.Request().ParseForm()
+	if formError != nil {
+		return studentController.responder.HTTP(400, strings.NewReader(formError.Error()))
+	}
+
+	form := req.Request().Form
+
+	student := &entity.Student{
+		Name:        funcs.ValidateStringFormKeys("Name", form, "string").(string),
+		Email:       funcs.ValidateStringFormKeys("Email", form, "string").(string),
+		MobilePhone: funcs.ValidateStringFormKeys("MobilePhone", form, "string").(string),
+		CreatedAt:   time.Now(),
+	}
+
+	//fmt.Printf("validator: %+v\n", studentController.validator.validate)
+	validateError := studentController.customValidator.Validate.Struct(student)
+	if validateError != nil {
+		errorResponse := funcs.ErrorPackagingForMaps(studentController.customValidator.TranslateError(validateError))
+		fmt.Println(errorResponse)
+		errorResponse, packError := funcs.ErrorPackaging(errorResponse, 400)
+		if packError != nil {
+			return studentController.responder.HTTP(500, strings.NewReader(packError.Error()))
+		}
+		return studentController.responder.HTTP(400, strings.NewReader(errorResponse))
+	}
+
+	result, err := studentController.studentService.CreateStudent(*student)
+	if err != nil {
+		fmt.Println(err)
+		errorResponse, packError := funcs.ErrorPackaging(err.Error(), 500)
+		if packError != nil {
+			return studentController.responder.HTTP(500, strings.NewReader(packError.Error()))
+		}
+		return studentController.responder.HTTP(500, strings.NewReader(errorResponse))
+	}
+
+	res, resErr := json.Marshal(result)
+	if resErr != nil {
+		return studentController.responder.HTTP(400, strings.NewReader(resErr.Error()))
+	}
+
+	return studentController.responder.HTTP(uint(result.Status), strings.NewReader(string(res)))
+
+}
+
+func (studentController *StudentController) Get(ctx context.Context, req *web.Request) web.Result {
+	if req.Params["id"] == "" {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  400,
+			Message: "Please select an student!",
+		})
+	}
+
+	intID, err := strconv.Atoi(req.Params["id"])
+	//PrintError(err)
+
+	if intID <= 0 {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  400,
+			Message: "Please select an student!",
+		})
+	}
+
+	student, err := studentController.studentService.GetStudent(intID)
+	if err != nil {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  500,
+			Message: "We cannot process your request. Please try again or contact support!",
+		})
+	}
+
+	if student.ID <= 0 {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  404,
+			Message: "student Not Found!",
+		})
+	}
+
+	return studentController.responder.Data(GetStudentResponse{
+		Status: 200,
+		Data:   student,
+	})
+}
+
+func (studentController *StudentController) Delete(ctx context.Context, req *web.Request) web.Result {
+	if req.Params["id"] == "" {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  400,
+			Message: "Please select an student!",
+		})
+	}
+
+	intID, err := strconv.Atoi(req.Params["id"])
+	//PrintError(err)
+
+	if intID <= 0 {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  400,
+			Message: "Please select an student!",
+		})
+	}
+
+	student, err := studentController.studentService.GetStudent(intID)
+	if err != nil {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  500,
+			Message: "We cannot process your request. Please try again or contact support!",
+		})
+	}
+
+	if student.ID <= 0 {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  404,
+			Message: "student Not Found!",
+		})
+	}
+
+	result, err := studentController.studentService.DeleteStudent(intID)
+	if err != nil {
+		errorResponse, packError := funcs.ErrorPackaging(err.Error(), 500)
+		if packError != nil {
+			return studentController.responder.HTTP(500, strings.NewReader(packError.Error()))
+		}
+		return studentController.responder.HTTP(500, strings.NewReader(errorResponse))
+	}
+
+	res, resErr := json.Marshal(result)
+	if resErr != nil {
+		return studentController.responder.HTTP(400, strings.NewReader(resErr.Error()))
+	}
+
+	return studentController.responder.HTTP(uint(result.Status), strings.NewReader(string(res)))
+}
+
+func (studentController *StudentController) Update(ctx context.Context, req *web.Request) web.Result {
+	if req.Params["id"] == "" {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  400,
+			Message: "Please select an student!",
+		})
+	}
+
+	intID, err := strconv.Atoi(req.Params["id"])
+	//PrintError(err)
+
+	if intID <= 0 {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  400,
+			Message: "Please select an student!",
+		})
+	}
+
+	student, err := studentController.studentService.GetStudent(intID)
+	if err != nil {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  500,
+			Message: "We cannot process your request. Please try again or contact support!",
+		})
+	}
+
+	if student.ID <= 0 {
+		return studentController.responder.Data(definitions.GenericAPIMessage{
+			Status:  404,
+			Message: "student Not Found!",
+		})
+	}
+
+	formError := req.Request().ParseForm()
+	if formError != nil {
+		return studentController.responder.HTTP(400, strings.NewReader(formError.Error()))
+	}
+
+	form := req.Request().Form
+
+	studentData := &entity.Student{
+		Name:        funcs.ValidateOrOverwriteStringFormKeys("Name", form, "string", student).(string),
+		Email:       funcs.ValidateOrOverwriteStringFormKeys("Email", form, "string", student).(string),
+		MobilePhone: funcs.ValidateOrOverwriteStringFormKeys("MobilePhone", form, "string", student).(string),
+		CreatedAt:   time.Now(),
+	}
+
+	//fmt.Printf("validator: %+v\n", studentController.validator.validate)
+	validateError := studentController.customValidator.Validate.Struct(studentData)
+	if validateError != nil {
+		errorResponse := funcs.ErrorPackagingForMaps(studentController.customValidator.TranslateError(validateError))
+		errorResponse, packError := funcs.ErrorPackaging(errorResponse, 400)
+		if packError != nil {
+			return studentController.responder.HTTP(500, strings.NewReader(packError.Error()))
+		}
+		return studentController.responder.HTTP(400, strings.NewReader(errorResponse))
+	}
+
+	result, err := studentController.studentService.UpdateStudent(intID, *studentData)
+	if err != nil {
+		fmt.Println(err)
+		errorResponse, packError := funcs.ErrorPackaging(err.Error(), 500)
+		if packError != nil {
+			return studentController.responder.HTTP(500, strings.NewReader(packError.Error()))
+		}
+		return studentController.responder.HTTP(500, strings.NewReader(errorResponse))
+	}
+
+	res, resErr := json.Marshal(result)
+	if resErr != nil {
+		return studentController.responder.HTTP(400, strings.NewReader(resErr.Error()))
+	}
+
+	return studentController.responder.HTTP(uint(result.Status), strings.NewReader(string(res)))
+}
+
+func (studentController *StudentController) GetAll(ctx context.Context, req *web.Request) web.Result {
+	query := req.QueryAll()
+	paginationReq := definitions.PaginationRequest{
+		SelectedColumns: funcs.ValidateStringFormKeys("select", query, "string").(string),
+		Search:          funcs.ValidateStringFormKeys("search", query, "string").(string),
+		Page:            funcs.ValidateStringFormKeys("page", query, "string").(string),
+		PerPage:         funcs.ValidateStringFormKeys("perpage", query, "string").(string),
+		OrderBy:         funcs.ValidateStringFormKeys("order", query, "string").(string),
+		Filter:          funcs.ValidateStringFormKeys("filter", query, "string").(string),
+	}
+
+	result, err := studentController.studentService.GetAllStudents(paginationReq)
+	if err != nil {
+		fmt.Println(err)
+		errorResponse, packError := funcs.ErrorPackaging(err.Error(), 500)
+		if packError != nil {
+			return studentController.responder.HTTP(500, strings.NewReader(packError.Error()))
+		}
+		return studentController.responder.HTTP(500, strings.NewReader(errorResponse))
+	}
+
+	res, resErr := json.Marshal(result)
+	if resErr != nil {
+		return studentController.responder.HTTP(400, strings.NewReader(resErr.Error()))
+	}
+
+	return studentController.responder.HTTP(uint(result.Status), strings.NewReader(string(res)))
+
+}
+
+func PrintError(err error) error {
+	fmt.Println(err)
+	if err != nil {
+		return fmt.Errorf("error: %v", err)
+	}
+	return nil
+}
--- api/routes.go
+++ api/routes.go
@@ -8,10 +8,15 @@ import (
 
 type Routes struct {
 	instructorController interfaces.InstructorController
+	studentController    interfaces.StudentController
 }
 
-func (routes *Routes) Inject(instructorController *interfaces.InstructorController) {
+func (routes *Routes) Inject(
+	instructorController *interfaces.InstructorController,
+	studentController *interfaces.StudentController,
+) {
 	routes.instructorController = *instructorController
+	routes.studentController = *studentController
 }
 
 func (routes *Routes) Routes(registry *web.RouterRegistry) {
@@ -25,4 +30,15 @@ func (routes *Routes) Routes(registry *web.RouterRegistry) {
 
 	registry.Route("/instructor-all/", "instructor-all")
 	registry.HandleGet("instructor-all", routes.instructorController.GetAll)
+
+	registry.Route("/student/:id", "student")
+	registry.HandleGet("student", routes.studentController.Get)
+	registry.HandlePut("student", routes.studentController.Update)
+	registry.HandleDelete("student", routes.studentController.Delete)
+
+	registry.Route("/student/", "student")
+	registry.HandlePost("student", routes.studentController.Create)
+
+	registry.Route("/student-all/", "student-all")
+	registry.HandleGet("student-all", routes.studentController.GetAll)
 }
