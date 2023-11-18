54bb9effb0d31cd6575e9a0d5f16c2a33b9ed908
ahmdisa23 <isa@easyparcel.com>
2023-11-18 15:51:17 +0700

    patch: add mail validation

--- api/services/userService.go
+++ api/services/userService.go
@@ -4,7 +4,9 @@ import (
 	"alfath_lms/api/definitions"
 	"alfath_lms/api/models"
 	"context"
+	"errors"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -18,15 +20,23 @@ func (userSvc *UserService) Inject(mongo *mongo.Database) {
 }
 
 func (userSvc *UserService) Create(User models.User) (definitions.GenericMongoCreationMessage, error) {
-	//return definitions.GenericCreationMessage{}, nil
-	insertResult, err := userSvc.mongo.Collection("users").InsertOne(context.Background(), User)
+	filter := bson.D{{"email", User.Email}}
+	searchResult := userSvc.mongo.Collection("users").FindOne(context.TODO(), filter)
+	if searchResult.Err() == mongo.ErrNoDocuments {
+		insertResult, err := userSvc.mongo.Collection("users").InsertOne(context.TODO(), User)
 
-	if err != nil {
-		return definitions.GenericMongoCreationMessage{}, nil
+		if err != nil {
+			return definitions.GenericMongoCreationMessage{}, nil
+		}
+
+		return definitions.GenericMongoCreationMessage{
+			Status:     200,
+			InstanceID: insertResult.InsertedID.(primitive.ObjectID),
+		}, nil
+	} else if searchResult.Err() != nil {
+		return definitions.GenericMongoCreationMessage{}, searchResult.Err()
+	} else {
+		return definitions.GenericMongoCreationMessage{}, errors.New("user with that email already exists")
 	}
 
-	return definitions.GenericMongoCreationMessage{
-		Status:     200,
-		InstanceID: insertResult.InsertedID.(primitive.ObjectID),
-	}, nil
 }
