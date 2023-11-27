3e7deabbf261dc19a2e015fadcb44665bb5798f4
ahmdisa23 <isa@easyparcel.com>
2023-11-28 06:02:53 +0700

    add: JWT Token pre 1.0!

--- api/middleware/authMiddleware.go
+++ api/middleware/authMiddleware.go
@@ -2,9 +2,12 @@ package middleware
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"strings"
 
 	"flamingo.me/flamingo/v3/framework/web"
+	"github.com/golang-jwt/jwt"
 )
 
 type AuthMiddleware struct {
@@ -15,9 +18,32 @@ func (authMdw *AuthMiddleware) AuthCheck(ctx context.Context, req *web.Request, 
 	headers := req.Request().Header
 	//get path = login
 
-	if headers["Authorization"] == nil {
+	auth, authOk := headers["Authorization"]
+	if !authOk {
 		return authMdw.Responder.HTTP(401, strings.NewReader("Not authorized!"))
 	} else {
+		auth := strings.Split(auth[0], " ")
+		token, err := jwt.Parse(auth[1], func(token *jwt.Token) (interface{}, error) {
+			val, ok := token.Method.(*jwt.SigningMethodHMAC)
+			fmt.Println(val)
+			if !ok {
+				return "Not authorized!", nil
+			}
+
+			return []byte(os.Getenv("JWT_KEY")), nil //Parse function must return a key. remember it's called the "Keyfunc".
+		})
+
+		if err != nil {
+			return authMdw.Responder.HTTP(401, strings.NewReader("Not authorized!"))
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if ok && token.Valid {
+			req.Request().Header.Add("email", claims["email"].(string))
+			req.Request().Header.Add("role_name", claims["role_name"].(string))
+		}
+
+		fmt.Println(req.Request().Header)
 		return action(ctx, req)
 	}
 
