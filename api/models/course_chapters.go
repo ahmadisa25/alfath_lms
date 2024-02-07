deeb5c0db90abfdc3d1bdd57854bc37b08b5160c
isadrianus <adrianus.isa@modena.com>
2024-02-07 12:55:58 +0700

    patch: model update

--- api/models/course_chapters.go
+++ api/models/course_chapters.go
@@ -8,16 +8,16 @@ import (
 
 type CourseChapter struct {
 	gorm.Model
-	ID          int         `gorm:"primaryKey" json:"ID,omitempty"`
-	Name        string      `validate:"required" json:"Name,omitempty"`
-	Description string      `validate:"required" json:"Description,omitempty"`
-	Duration    int         `validate:"required,numeric" json:"Duration,omitempty"`
-	CreatedAt   time.Time   `gorm:"default:NULL" json:"CreatedAt,omitempty"`
-	UpdatedAt   time.Time   `json:"UpdatedAt,omitempty"`
-	DeletedAt   time.Time   `gorm:"default:NULL" json:"DeletedAt,omitempty"`
-	Course      Course      `validate:"omitempty" json:"Course,omitempty"`
-	CourseID    int         `gorm:"foreignKey:CourseID" validate:"required" json:"CourseID,omitempty"`
-	Materials   []Materials `json:"Materials,omitempty"`
+	ID          int                `gorm:"primaryKey" json:"ID,omitempty"`
+	Name        string             `validate:"required" json:"Name,omitempty"`
+	Description string             `validate:"required" json:"Description,omitempty"`
+	Duration    int                `validate:"required,numeric" json:"Duration,omitempty"`
+	CreatedAt   time.Time          `gorm:"default:NULL" json:"CreatedAt,omitempty"`
+	UpdatedAt   time.Time          `json:"UpdatedAt,omitempty"`
+	DeletedAt   time.Time          `gorm:"default:NULL" json:"DeletedAt,omitempty"`
+	Course      Course             `validate:"omitempty" json:"Course,omitempty"`
+	CourseID    int                `gorm:"foreignKey:CourseID" validate:"required" json:"CourseID,omitempty"`
+	Materials   []*ChapterMaterial `json:"Materials,omitempty"`
 }
 
 // TableName overrides the table name used by User to `profiles`
