ec3ca0d5d07455638b0867e4587722b9865bc0c3
isadrianus <adrianus.isa@modena.com>
2024-02-17 10:20:13 +0700

    patch: add questions to quiz model

--- api/models/chapter_quiz.go
+++ api/models/chapter_quiz.go
@@ -8,15 +8,16 @@ import (
 
 type ChapterQuiz struct {
 	gorm.Model
-	ID              int           `gorm:"primaryKey" json:"ID,omitempty"`
-	Name            string        `validate:"required" json:"Name,omitempty"`
-	Description     string        `validate:"required" json:"Description,omitempty"`
-	Duration        int           `validate:"required,numeric" json:"Duration,omitempty"`
-	CreatedAt       time.Time     `gorm:"default:NULL" json:"CreatedAt,omitempty"`
-	UpdatedAt       time.Time     `json:"UpdatedAt,omitempty"`
-	DeletedAt       time.Time     `gorm:"default:NULL" json:"DeletedAt,omitempty"`
-	CourseChapter   CourseChapter `validate:"omitempty" json:"CourseChapter,omitempty"`
-	CourseChapterID int           `gorm:"foreignKey:CourseChapterID" validate:"required" json:"CourseChapterID,omitempty"`
+	ID              int             `gorm:"primaryKey" json:"ID,omitempty"`
+	Name            string          `validate:"required" json:"Name,omitempty"`
+	Description     string          `validate:"required" json:"Description,omitempty"`
+	Duration        int             `validate:"required,numeric" json:"Duration,omitempty"`
+	CreatedAt       time.Time       `gorm:"default:NULL" json:"CreatedAt,omitempty"`
+	UpdatedAt       time.Time       `json:"UpdatedAt,omitempty"`
+	DeletedAt       time.Time       `gorm:"default:NULL" json:"DeletedAt,omitempty"`
+	CourseChapter   CourseChapter   `validate:"omitempty" json:"CourseChapter,omitempty"`
+	CourseChapterID int             `gorm:"foreignKey:CourseChapterID" validate:"required" json:"CourseChapterID,omitempty"`
+	Questions       []*QuizQuestion `json:"Questions,omitempty"`
 }
 
 // TableName overrides the table name used by User to `profiles`
