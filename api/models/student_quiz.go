6561c608d4d8a13a11756ec173d2a8dee88055c5
isadrianus <adrianus.isa@modena.com>
2024-02-23 06:11:00 +0700

    patch: add improvement on quiz_answers

--- api/models/student_quiz.go
+++ api/models/student_quiz.go
@@ -8,14 +8,15 @@ import (
 
 type StudentQuiz struct {
 	gorm.Model
-	ID            int         `gorm:"primaryKey" json:"ID,omitempty"`
-	Student       Student     `validate:"omitempty" json:"Student,omitempty"`
-	StudentID     int         `gorm:"foreignKey:StudentID" validate:"required" json:"StudentID,omitempty"`
-	ChapterQuiz   ChapterQuiz `validate:"omitempty" json:"ChapterQuiz,omitempty"`
-	ChapterQuizID int         `gorm:"foreignKey:ChapterQuizID" validate:"required" json:"ChapterQuizID,omitempty"`
-	CreatedAt     time.Time   `gorm:"default:NULL" json:"CreatedAt,omitempty"`
-	UpdatedAt     time.Time   `json:"UpdatedAt,omitempty"`
-	DeletedAt     time.Time   `gorm:"default:NULL" json:"DeletedAt,omitempty"`
+	ID         int     `gorm:"primaryKey" json:"ID,omitempty"`
+	Student    Student `validate:"omitempty" json:"Student,omitempty"`
+	StudentID  int     `gorm:"foreignKey:StudentID" validate:"required" json:"StudentID,omitempty"`
+	FinalGrade int     `json:"FinalGrade,omitempty"`
+	h
+	CreatedAt time.Time     `gorm:"default:NULL" json:"CreatedAt,omitempty"`
+	UpdatedAt time.Time     `json:"UpdatedAt,omitempty"`
+	DeletedAt time.Time     `gorm:"default:NULL" json:"DeletedAt,omitempty"`
+	Answers   []*QuizAnswer `validate: "required" json:"Answers"`
 }
 
 // TableName overrides the table name used by User to `profiles`
